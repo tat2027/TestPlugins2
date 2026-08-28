e2e1c462f7137ab6e2cb295dc67f01fe4bac19c5
M <102477479+tat2027@users.noreply.github.com>
2026-08-28 12:14:31 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.7.3")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
 }
 
@@ -24,6 +24,11 @@ allprojects {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs = freeCompilerArgs + listOf("-Xskip-metadata-version-check")
+        }
+    }
     }
 }
 
